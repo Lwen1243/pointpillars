835731ffba599e8a442ad6a3ab9adfe3e44ecc06
Lwen1243 <1198807618@qq.com>
2026-03-05 11:09:24 +0800

    update bug

--- src/core/geometry.py
+++ src/core/geometry.py
@@ -25,7 +25,7 @@ def surface_equ_3d_jit(polygon_surfaces):
     surface_vec = polygon_surfaces[:, :, :2, :] - polygon_surfaces[:, :, 1:3, :]
     # normal_vec: [..., 3]
     normal_vec = np.cross(surface_vec[:, :, 0, :], surface_vec[:, :, 1, :])
-    d = np.einsum('aij, aij->ai', normal_vec, polygon_surfaces[:, :, 0, :])
+    d = (normal_vec * polygon_surfaces[:, :, 0, :]).sum(axis=-1)
     return normal_vec, -d
 
 
